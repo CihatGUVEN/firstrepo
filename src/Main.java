44c8554ec65f7e413a35b07506721cd538f2d0c2
CihatGUVEN <89444171+CihatGUVEN@users.noreply.github.com>
2021-09-30 10:07:45 +0300

    yeni proje eklendi

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	String word = "cihat";
+    String wordstar = " ";
+        for(int i=0;i<word.length();i++){
+           String s = "*"+ word.charAt(i);
+           wordstar =wordstar+ s;
+        }
+        System.out.println(wordstar);
+    }
+}
